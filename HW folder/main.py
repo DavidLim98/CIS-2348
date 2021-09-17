0e0ed63aba1212fa18b8d1ac1a1e7b96a6c7e29f
DavidLim98 <90165816+DavidLim98@users.noreply.github.com>
2021-09-17 17:09:35 -0500

    Add files via upload

---
+++ HW folder/main.py
@@ -0,0 +1,17 @@
+user_num1 = int(input())
+
+# Type your code here
+print ('Enter integer:')
+
+print ('You entered:', user_num1)
+
+print (user_num1, 'squared is', (user_num1*user_num1))
+
+print ('And', user_num1, 'cubed is', (user_num1*user_num1*user_num1), '!!')
+
+user_num2 = int(input())
+
+print ('Enter another integer:')
+
+print (user_num1, '+', user_num2, 'is', (user_num1+user_num2))
+print (user_num1, '*', user_num2, 'is', (user_num1*user_num2))
