cb0c79279af7f640ac103e917c7d75c0284ea53b
DavidLim98 <90165816+DavidLim98@users.noreply.github.com>
2021-09-17 17:16:55 -0500

    Update and rename main.py to zylab 1.12.py

---
+++ HW folder/zylab 1.12.py
@@ -0,0 +1,6 @@
+# David Lim, 1625277
+
+userNum = int(input())
+userNumSquared = userNum * userNum  # Bug here; fix it when instructed
+
+print(userNumSquared)  # Output formatting issue here; fix it when instructed
