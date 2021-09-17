baac54037c7ec1547f5d32bcb843afdc017f3f15
DavidLim98 <90165816+DavidLim98@users.noreply.github.com>
2021-09-17 18:24:28 -0500

    Update and rename main.py to zylab 5.22.py

--- HW folder/zylab 5.22.py
+++ HW folder/zylab 5.22.py
@@ -1,3 +1,5 @@
+# David Lim, 1625277
+
 print("Davy's auto shop services\nOil change -- $35\nTire rotation -- $19\nCar wash -- $7\nCar wax -- $12")
 print('')
 shopService1=input("Select first service:\n")
