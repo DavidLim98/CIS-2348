d852a22592778860c254b3c1306579941febca05
DavidLim98 <90165816+DavidLim98@users.noreply.github.com>
2021-10-13 10:10:06 -0500

    Update HW2.py

--- HW folder/HW2.py
+++ HW folder/HW2.py
@@ -1,3 +1,5 @@
+# David Lim, 1625277
+
 month_list= {'january':'1','february':'2','march':'3','april':'4','may':'5','june':'6','july':'7','august':'8','september':'9','october':'10','november':'11','december':'12'}
 
 input_file=open('C:\\Users\\david\\Desktop\\inputDates.txt','r')
