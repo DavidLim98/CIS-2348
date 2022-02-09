e67934f210dc8574459394559c58b73850260a1d
DavidLim98 <90165816+DavidLim98@users.noreply.github.com>
2022-02-09 01:42:29 -0600

    Update zylab 3.19.py

--- HW folder/zylab 3.19.py
+++ HW folder/zylab 3.19.py
@@ -1,18 +1,25 @@
 # David Lim, 1625277
 
 import math
-wallHeight = float(input("Enter wall height (feet):\n"))
-wallWidth = float(input("Enter wall width (feet):\n"))
-area = wallHeight*wallWidth
-print("Wall area:",int(area),"square feet")
-paintNeeded = area/350
-print("Paint needed: %0.2f gallons"%paintNeeded)
-print("Cans needed:",math.ceil(paintNeeded),"can(s)")
-paintColors = {
-    'red': 35,
-    'blue': 25,
-    'green':23
-}
-color = input("\nChoose a color to paint the wall:\n")
-print("Cost of purchasing",color,"paint:")
-print("$",str(paintColors[color]))
+
+paintColors = {'red':35, 'blue':25, 'green':23}
+
+wallHeight = int(input("Enter wall height (feet):\n"))
+
+wallWidth = int(input("Enter wall width (feet):\n"))
+
+wall_area = (wallHeight*wallWidth)
+
+paint_needed = (wall_area/350)
+
+print("Wall area:",wall_area,'square feet')
+
+print("Paint needed: {:.2f} gallons".format(paint_needed))
+
+cans = math.ceil(paint_needed)
+
+print("Cans needed:", cans,'can(s)')
+
+color = input('\nChoose a color to paint the wall:\n')
+
+print("Cost of purchasing",color,"paint: $"+str(cans*paintColors[color]))
